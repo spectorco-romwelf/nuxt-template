e508f8af5d13811b63c53f81c0553035c3646d6e
Romwel Feliciano <romwel@spectorandco.com>
2025-10-03 01:19:31 +0800

    color-mode: light theme default

---
+++ app/pages/index.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <div>Index Page</div>
+</template>
+
+<style scoped></style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2025-07-15",
+  compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint", "@nuxt/image", "@nuxt/ui", "@nuxt/scripts"],
-  css: ["~/assets/css/main.css"],
+  modules: ['@nuxt/eslint', '@nuxt/image', '@nuxt/ui', '@nuxt/scripts'],
+  css: ['~/assets/css/main.css'],
+  colorMode: {
+    preference: 'light',
+    fallback: 'light',
+    classSuffix: '',
+    storage: 'sessionStorage',
+  },
 });
