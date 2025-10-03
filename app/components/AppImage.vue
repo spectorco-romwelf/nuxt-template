3049b80d7a2c689e329fe5a686e5de617048284e
Romwel Feliciano <romwel@spectorandco.com>
2025-10-04 01:51:32 +0800

    component: app image

---
+++ app/components/AppImage.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineProps({
+  src: String,
+  width: String,
+  height: String,
+  alt: String,
+});
+</script>
+
+<template>
+  <NuxtImg :src="src" :key="src" :alt="alt" :width="width" :height="height" :placeholder="[width, height]" loading="lazy" />
+</template>
+
+<style scoped></style>
---
+++ app/layouts/default.vue
@@ -0,0 +1,12 @@
+<script setup></script>
+
+<template>
+  <header>
+    <AppImage src="/logo.png" alt="Spector and Co. Logo" width="214" height="70" />
+    <nav>Nav</nav>
+  </header>
+  <main>
+    <slot></slot>
+  </main>
+  <footer>Footer</footer>
+</template>
